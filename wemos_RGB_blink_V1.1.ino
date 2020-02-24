e1f428c64649395c17052f1d338cf7871c7d91dd
Pelikano <leon@berkenheuvel.com>
2020-02-24 20:25:24 +0100

    simpel blink

---
+++ wemos_RGB_blink_V1.1.ino
@@ -0,0 +1,36 @@
+void BlinkLED(); // BlinkLED(color, times, milliseconds);
+
+//RGB led wemos
+int Green = 14; // D5 = GPIO14
+int Red = 12; // D6 = GPIO12
+int Blue = 13; // D7 = GPIO13
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+  pinMode(Blue, OUTPUT);    // Initialize the LED pin as an output
+  pinMode(Red, OUTPUT);     // Initialize the LED pin as an output
+  pinMode(Green, OUTPUT);   // Initialize the LED pin as an output
+  digitalWrite(Blue, HIGH);
+  digitalWrite(Red, HIGH);
+  digitalWrite(Green, HIGH);
+
+}
+
+void loop() {
+  // BlinkLED(Green, 1, 100);
+  //    delay(100);
+ // BlinkLED(Blue, 1, 100);
+  //    delay(100);
+      BlinkLED(Red, 1, 100);
+  //    delay(100);
+  //    BlinkLED(BUILTIN_LED, 1, 100);
+  //    delay(100);
+}
+
+void BlinkLED(uint8_t color, int times, int milliseconds)
+{
+  for (int i = 1; i < times + 1 ; i++ ) {
+    digitalWrite(color, !digitalRead(color));
+    delay(milliseconds);
+  }
+}
